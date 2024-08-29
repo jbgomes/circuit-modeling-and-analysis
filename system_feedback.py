9dfdbf829dce14fd8d271a19dbb409eafb3dbae5
Julia Gomes <juliatb.gomes@gmail.com>
2024-08-29 15:34:25 -0300

    feat: add circuit feedback simulation

--- system_feedback.py
+++ system_feedback.py
@@ -1,9 +1,18 @@
 from control.matlab import *
-import matplotlib.pyplot as plt 
 
-s = tf('s')
+H_1=tf (1 ,[1 ,2])
+H_2=tf (1 ,[1 ,3])
 
-H = (s+3)/((s+2)*((s+1)**2+2**2))
-print(H)
+H_parallel=parallel(H_1,H_2)
+print('Parallel: ', H_parallel)
 
-print('dc gain:', dcgain(H))
+H_serie=series(H_1,H_2)
+print('Serie:', H_serie)
+
+negative_feedback=feedback(H_1,H_2, sign=-1) # H1 é o ramo direto e H2 é o ramo de realimentacao
+print('Negative feedback:', negative_feedback)
+print('dc gain:', dcgain(negative_feedback))
+
+positive_feedback=feedback(H_1,H_2, sign=+1) # H1 é o ramo direto e H2 é o ramo de realimentacao
+print('Positive feedback', positive_feedback)
+print('dc gain:', dcgain(positive_feedback))

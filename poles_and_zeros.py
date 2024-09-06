196ea1bb08a96b7e8aaebaa446cb4039d6b5f88f
Julia Gomes <juliatb.gomes@gmail.com>
2024-09-06 09:58:30 -0300

    style: reduce poles and zero function

--- poles_and_zeros.py
+++ poles_and_zeros.py
@@ -12,10 +12,6 @@ def pole_zero(H):
     # rlocus(H)
     plt.show()
 
-def time_response_info(H):
-    S = stepinfo(H, SettlingTimeThreshold=0.01, RiseTimeLimits=(0.0, 1.0))
-    print(S)
-
 #example
 s = tf('s')
 pole_zero(H = (s+3)/((s+2)*((s+1)**2+2**2)))

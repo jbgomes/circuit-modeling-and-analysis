e27252e3a3c4735ec103461b0202099707e6321e
Julia Gomes <juliatb.gomes@gmail.com>
2024-09-10 15:34:58 -0300

    add: add open loop test

--- test_opamp-model-simulation.py
+++ test_opamp-model-simulation.py
@@ -1,7 +1,9 @@
 from laplace_inverse import i_laplace
 from frequency_response import bode2
+from math import log10
 
-k = 10
+# k = 10 closed loop
+k = 1e5 # open loop
 R1 = 1e5
 R2 = k*R1
 Cl = 1e-12
@@ -9,12 +11,20 @@ Cg = 0.5e-12
 gm = 100e-6
 r0 = 1e7
 
-#num = [-R2*r0*(gm*R2 - 1)]
-#den = [(-r0*R1*Cg*Cl*R2**2), (r0*Cl*R2**2 + R1*Cg*R2**2 + r0*R1*R2*Cg + r0*R1*R2*Cl), (R2**2 + R1*R2 + r0*R2 + R1*r0*gm*R2)]
+num = [-R2*r0*(gm*R2 - 1)]
+den = [(-r0*R1*Cg*Cl*R2**2), (r0*Cl*R2**2 + R1*Cg*R2**2 + r0*R1*R2*Cg + r0*R1*R2*Cl), (R2**2 + R1*R2 + r0*R2 + R1*r0*gm*R2)]
 
 # open loop test
-num = gm*r0
-den = 1
+#print('loop open gain: ', gm*r0)
+print('loop open gain: ', 20*log10(gm*r0),'dB')
 
-bode2(num, den, 1e5, 1e9, 1000)
+bode2(num, den, 0.1, 1e9, 1000)
 #i_laplace(num, den)
+
+#closed loop
+#f0 = 1e5
+#ff = 1e9
+
+#open loop
+#f0 = 0.1
+#ff = 1e9
